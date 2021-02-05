8930e25b37608218e84ddb26299d1b33c0bfc758
Koji Takami <vzg03566@gmail.com>
2021-02-06 08:41:02 +0900

    Implement a validation for Language constructor
    This commit will close the issue #16

--- test/syntax-rule.test.js
+++ test/syntax-rule.test.js
@@ -6,5 +6,20 @@ describe("SyntaxRule", ()=>{
         it("should throw if the type of element of the rules are illegal", ()=>{
             assert.throw(()=>(new SyntaxRule("should be thrown", [[1]])));
         });
+        it("should throw if the rule is empty", ()=>{
+            assert.throw(()=>(new SyntaxRule("should be thrown", [])));
+        });
+        it("should throw if the element of rule is empty", ()=>{
+            assert.throw(()=>(new SyntaxRule("should be thrown", [[]])));
+            assert.throw(()=>(new SyntaxRule("should be thrown", [["a"],[]])));
+        });
+        it("should throw if it configures left-recursion", ()=>{
+            assert.throw(()=>(new SyntaxRule("left-recursion", [
+                ["left-recursion"]])));
+            assert.throw(()=>(new SyntaxRule("left-recursion", [
+                ["a"],["left-recursion"]])));
+            assert.throw(()=>(new SyntaxRule("left-recursion", [
+                ["a"],["left-recursion", "a"]])));
+        });
     });
 });
